4f84f2552c8faf4ed163110caf40e115adfc08f2
John Stein <john.stein@fireeye.com>
2018-10-10 18:40:38 -0400

    hw

---
+++ README.md
@@ -0,0 +1,22 @@
+# cdrop
+
+POST a file to cdrop. Then you can GET it once.
+After that, it's deleted.
+
+Makes it easy to share files with relative security.
+
+## POST a file to cdrop.
+
+```sh
+curl -F 'file=@README.md' http://localhost:8080
+<a href="http://localhost:8080/evi2_DA_v">http://localhost:8080/evi2_DA_v</a>
+localhost:8080/evi2_DA_v
+```
+
+## GET the file from cdrop.
+
+```sh
+curl http://localhost:8080/evi2_DA_v > README-copy.md
+# OR
+wget -o README-copy.md http://localhost:8080/evi2_DA_v
+```
---
+++ cdrop.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var formidable = require('formidable');
+var shortid = require('shortid');
+var fs = require('fs');
+var cache = {};
+http.createServer(function(req, res) {
+  if (req.method === 'POST') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+      if (err) {
+        res.statusCode = 500;
+        res.write('ooops');
+        res.end();
+      } else {
+        var id = shortid.generate();
+        cache[id] = files.file.path;
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        res.write(`<a href="http://${req.headers.host}/${id}">http://${req.headers.host}/${id}</a>\n${req.headers.host}/${id}\n`);
+        res.end();
+      }
+    });
+  } else if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.write('<form action="/" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="file"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    res.end();
+  } else {
+    var id = req.url.substring(1);
+    if (cache[id]) {
+      var stat = fs.statSync(cache[id]);
+      res.writeHead(200, {
+        'Content-Disposition': 'attachment; filename="' + cache[id] + '"',
+        'Content-Length': stat.size
+      });
+      fs.createReadStream(cache[id])
+        .pipe(res)
+        .on('finish', () => {
+          console.log('deleting', id, cache);
+          fs.unlinkSync(cache[id]);
+          delete cache[id];
+        });
+    } else {
+      res.statusCode = 404;
+      res.write('you are lost');
+      res.end();
+    }
+  }
+}).listen(8080);
